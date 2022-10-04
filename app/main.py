4be34d69703926afbafa4ac2e08b52ccc14b4ee5
DKKs <eye.q.21@gmail.com>
2022-10-04 14:48:28 +0700

    [Add] README.md , example jinja template

--- app/main.py
+++ app/main.py
@@ -88,6 +88,7 @@ async def read_item(request: Request):
         'accounts.html',
         {
             'request': request,
-            'message': 'test'
+            'message': 'test',
+            'message_list': [1,2,3]
         }
     )
